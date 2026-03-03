98bbc98085c576c63d13d2babac3db033bfff19d
kalai02484 <kalai02484@gmail.com>
2026-03-03 08:47:00 +0530

    jwt sign added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,9 @@
 import User from "../models/userSchema.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const registerUser = async (req, res) => {
   try {
@@ -32,7 +36,7 @@ export const registerUser = async (req, res) => {
   }
 };
 
-export default loginUser = async (req, res) => {
+export const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
@@ -45,9 +49,21 @@ export default loginUser = async (req, res) => {
         .status(404)
         .json({ message: "Password Mismatch, Please Enter Correct Password" });
     }
+
+    //jwt token generation
+    const token = jwt.sign(
+      { _id: existingUser._id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1h" },
+    );
+
+    existingUser.token = token;
+
+    await existingUser.save();
+
     res
       .status(200)
-      .json({ message: "User Login Successfull", data: existingUser });
+      .json({ message: "User Login Successfull", token: token });
   } catch (error) {
     res.status(503).json({
       message: "Server Error, Unable to Login" || error.message,
